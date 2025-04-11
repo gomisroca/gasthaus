9755095f8cce0e8385586bdaf338bd68af3869d4
asfalfa <glkin9@gmail.com>
2025-04-11 11:03:07 +0200

    fix: missing landing import

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { SpeisekarteComponent } from './speisekarte/speisekarte.component';
 import { KontaktComponent } from './kontakt/kontakt.component';
+import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'root',
-  imports: [SpeisekarteComponent, KontaktComponent],
+  imports: [SpeisekarteComponent, KontaktComponent, LandingComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
