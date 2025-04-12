debf200d7f22f7aa6438865548325676f1822965
asfalfa <glkin9@gmail.com>
2025-04-12 15:07:50 +0200

    feat: category menu and item display

--- src/app/components/speisekarte/speisekarte.component.ts
+++ src/app/components/speisekarte/speisekarte.component.ts
@@ -1,12 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { SpeisekarteService } from '../../services/speisekarte.service';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'speisekarte',
-  imports: [],
+  imports: [ItemComponent],
   templateUrl: './speisekarte.component.html',
 })
 export class SpeisekarteComponent {
   private speisekarteService = inject(SpeisekarteService);
-  speisekarte = this.speisekarteService.getData();
+  categories = this.speisekarteService.getCategories();
+
+  category = signal<string>('Saisonal');
+  items = computed<
+    {
+      id: number;
+      name: string;
+      description: string;
+      categories: string[];
+      tags: string[];
+      price: number;
+    }[]
+  >(() => this.speisekarteService.getCategory(this.category()));
+
+  setCategory(category: string) {
+    this.category.update(() => category);
+  }
 }
