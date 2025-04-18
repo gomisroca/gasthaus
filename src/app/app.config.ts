3d2cadbad30f21eaf27e64da39d98c5a16f00f3f
asfalfa <glkin9@gmail.com>
2025-04-18 13:27:15 +0200

    routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,11 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "gasthaus-bb42f", appId: "1:1014010648331:web:39016c0682180803b9740e", storageBucket: "gasthaus-bb42f.firebasestorage.app", apiKey: "AIzaSyDa_wyAdg69NUGJ5H8rP4kwrl1MC_Dk5DE", authDomain: "gasthaus-bb42f.firebaseapp.com", messagingSenderId: "1014010648331", measurementId: "G-9WFT4PCLXN" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService,
+    provideClientHydration(withEventReplay()),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './components/app.component';
 
-export const routes: Routes = [{ path: '/', component: AppComponent }];
+export const routes: Routes = [{ path: '', component: AppComponent }];
