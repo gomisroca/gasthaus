ecccd86e611cda7923a867ecf1327ee9230033a9
asfalfa <glkin9@gmail.com>
2025-04-18 13:47:51 +0200

    remove firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './components/app.component';
+import { SpeisekarteComponent } from './components/speisekarte/speisekarte.component';
 
-export const routes: Routes = [{ path: '', component: AppComponent }];
+export const routes: Routes = [
+  { path: 'speisekarte', component: SpeisekarteComponent },
+];
