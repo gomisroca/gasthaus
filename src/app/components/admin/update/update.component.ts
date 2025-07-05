523346d055b36c10353c3e2cd0d2bbb23528462b
Adrià Gomis <gomisroca@gmail.com>
2025-07-05 09:59:21 +0200

    feat: update item logic, with optional image update

---
+++ src/app/components/admin/update/update.component.ts
@@ -0,0 +1,101 @@
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { SpeisekarteService } from '../../../services/speisekarte.service';
+import { SpeisekarteItem } from '../../../../../types';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'update-item',
+  imports: [ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './update.component.html',
+})
+export class UpdateComponent {
+  private activatedRoute = inject(ActivatedRoute);
+
+  form: FormGroup;
+  id = signal<string>('');
+  readonly item = signal<SpeisekarteItem>({
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+    categories: [],
+    tags: [],
+    seasonal: false,
+    image: null,
+  });
+
+  constructor(private fb: FormBuilder, private service: SpeisekarteService) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.id.set(params['id']);
+    });
+
+    this.form = this.fb.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      categories: [[] as string[]],
+      tags: [[] as string[]],
+      seasonal: [false],
+      image: [null],
+      currentImage: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    // Fetch item data
+    this.service.getUniqueItem(this.id()).subscribe({
+      next: (item) => {
+        this.item.set(item);
+        this.form.patchValue({
+          ...item,
+          currentImage: item.image,
+        });
+      },
+      error: (err) => console.error('Failed to load item', err),
+    });
+  }
+
+  onCategoriesInput(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    const categories = input
+      .split(',')
+      .map((s) => s.trim())
+      .filter((s) => s !== '');
+
+    this.form.patchValue({ categories });
+  }
+
+  onTagsInput(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    const tags = input
+      .split(',')
+      .map((s) => s.trim())
+      .filter((s) => s !== '');
+
+    this.form.patchValue({ tags });
+  }
+
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input?.files?.length) {
+      this.form.patchValue({ image: input.files[0] });
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.service.updateItem(this.form.value as any).subscribe({
+        next: () => alert('Item added!'),
+        error: (err) => console.error('Add failed:', err),
+      });
+    }
+  }
+}
--- src/app/services/speisekarte.service.ts
+++ src/app/services/speisekarte.service.ts
@@ -22,6 +22,10 @@ export class SpeisekarteService {
 
   constructor(private http: HttpClient) {}
 
+  getUniqueItem(id: string): Observable<SpeisekarteItem> {
+    return this.http.get<SpeisekarteItem>(`${this.apiUrl}/speisekarte/${id}`);
+  }
+
   getItems(): Observable<SpeisekarteItem[]> {
     return this.http.get<SpeisekarteItem[]>(`${this.apiUrl}/speisekarte/`);
   }
@@ -49,6 +53,34 @@ export class SpeisekarteService {
     formData.append('image', item.image);
 
     return this.http.post(`${this.apiUrl}/speisekarte/`, formData, {
+      withCredentials: true,
+    });
+  }
+
+  updateItem(
+    item: { id: string; currentImage: string } & NewSpeisekarteItem
+  ): Observable<any> {
+    const formData = new FormData();
+    formData.append('name', item.name);
+    formData.append('description', item.description);
+    formData.append('price', item.price.toString());
+
+    item.categories.forEach((cat) => formData.append('categories', cat));
+    item.tags.forEach((tag) => formData.append('tags', tag));
+
+    formData.append('seasonal', item.seasonal.toString());
+    formData.append('currentImage', item.currentImage);
+    if (item.image instanceof File) {
+      formData.append('image', item.image);
+    }
+
+    return this.http.put(`${this.apiUrl}/speisekarte/${item.id}`, formData, {
+      withCredentials: true,
+    });
+  }
+
+  deleteItem(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/speisekarte/${id}`, {
       withCredentials: true,
     });
   }
