420812c4b8632ba9468e46d5e1f77bcb3c6e8f9e
Adrià Gomis <gomisroca@gmail.com>
2025-07-11 15:47:18 +0200

    fix: make eslint play nice with html files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,63 +1,83 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    extraFileExtensions: ['.html'],
   },
-  "plugins": [
-    "@typescript-eslint",
-    "prettier",
-    "@angular-eslint",
-    "simple-import-sort",
-    "import"
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    '@angular-eslint',
+    'simple-import-sort',
+    'import',
   ],
-  "extends": [
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
+  extends: [
+    'plugin:prettier/recommended',
   ],
-  "rules": {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "import/first": "error",
-    "import/newline-after-import": "error",
-    "import/no-duplicates": "error",
-    "prettier/prettier": "error",
-    "no-console": "warn",
-    "@angular-eslint/directive-selector": [
-      "error",
-      {
-        type: "attribute",
-        prefix: "app",
-        style: "camelCase",
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+      ],
+      rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        'no-console': 'warn',
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/require-await': 'off',
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: {
+              attributes: false,
+            },
+          },
+        ],
       },
-    ],
-    "@angular-eslint/component-selector": [
-      "error",
-      {
-        type: "element",
-        prefix: "app",
-        style: "kebab-case",
+    },
+    {
+      files: ['*.html'],
+      parser: '@angular-eslint/template-parser',
+      plugins: ['@angular-eslint/template'],
+      rules: {
+        '@angular-eslint/template/no-negated-async': 'warn',
       },
-    ],
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
- 
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ],
-  }
-}
+    },
+  ],
+};
+
 module.exports = config;
