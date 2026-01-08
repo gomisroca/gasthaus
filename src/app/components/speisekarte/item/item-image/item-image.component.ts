d3309c3f2713bd8fa5ee644b408c8a4499f0644d
Adrià Gomis <gomisroca@gmail.com>
2026-01-08 22:03:21 +0100

    slight changes to item styling

--- src/app/components/speisekarte/item/item-image/item-image.component.ts
+++ src/app/components/speisekarte/item/item-image/item-image.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-image',
-  template: `<img [src]="imageSrc" alt="{{ name }}" (error)="onImageError($event)" />`,
+  template: `<img
+    [src]="imageSrc"
+    alt="{{ name }}"
+    (error)="onImageError($event)"
+    class="h-32 w-32 rounded-xl border-2 border-white/10 object-cover" />`,
 })
 export class ItemImageComponent {
   @Input() name!: string;
