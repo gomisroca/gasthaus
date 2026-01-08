068a9a71434d1a3324b9cb06ba762e4dccf9d61d
Adrià Gomis <gomisroca@gmail.com>
2026-01-08 17:19:03 +0100

    update server bootstrap fn

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { renderApplication } from '@angular/platform-server';
 
 import { config } from './app/app.config.server';
 import { AppComponent } from './app/components/app.component';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+export default function bootstrapServer(url?: string) {
+  return renderApplication(
+    (context) => bootstrapApplication(AppComponent, { ...config, ...context }),
+    { url } // optional URL for SSR
+  );
+}
