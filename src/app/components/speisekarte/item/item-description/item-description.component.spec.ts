18810767177edb650329d5ac671b344c72c419bf
Adrià Gomis <gomisroca@gmail.com>
2026-01-09 17:57:54 +0100

    fix item description test

--- src/app/components/speisekarte/item/item-description/item-description.component.spec.ts
+++ src/app/components/speisekarte/item/item-description/item-description.component.spec.ts
@@ -1,28 +1,20 @@
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { ItemDescriptionComponent } from './item-description.component';
 
-@Component({
-  standalone: true,
-  imports: [ItemDescriptionComponent],
-  templateUrl: './item-description.component.html',
-})
-class TestComponent {}
-
 describe('ItemDescriptionComponent', () => {
   let component: ItemDescriptionComponent;
-  let fixture: ComponentFixture<TestComponent>;
+  let fixture: ComponentFixture<ItemDescriptionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TestComponent],
+      imports: [ItemDescriptionComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TestComponent);
-    const itemTagDebugEl = fixture.debugElement.query(By.directive(ItemDescriptionComponent));
-    component = itemTagDebugEl.componentInstance as ItemDescriptionComponent;
+    fixture = TestBed.createComponent(ItemDescriptionComponent);
+    const itemDescriptionDebugEl = fixture.debugElement.query(By.directive(ItemDescriptionComponent));
+    component = itemDescriptionDebugEl.componentInstance as ItemDescriptionComponent;
     fixture.detectChanges();
   });
 
