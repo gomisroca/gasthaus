8a0d37317281e0a260d5641f54dde54d608147b8
Adrià Gomis <gomisroca@gmail.com>
2026-04-08 09:49:34 +0200

    use reusable btn class

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.component.html',
 })
 export class LandingComponent {}
