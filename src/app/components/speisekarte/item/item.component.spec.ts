030231c70989d220d89720d88bc12476063df6bd
Adrià Gomis <gomisroca@gmail.com>
2026-04-26 11:25:36 +0200

    fix tests

--- src/app/components/speisekarte/item/item.component.spec.ts
+++ src/app/components/speisekarte/item/item.component.spec.ts
@@ -1,8 +1,12 @@
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
 import { Component, LOCALE_ID } from '@angular/core';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { ItemComponent } from './item.component';
+
+registerLocaleData(localeDe);
 
 @Component({
   imports: [ItemComponent],
